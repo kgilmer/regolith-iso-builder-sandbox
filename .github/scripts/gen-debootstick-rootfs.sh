2746f8390b5adbd0a7ac56ed357ae9dff16077bf
Ken Gilmer <kgilmer@gmail.com>
2025-09-30 18:03:54 -0700

    Extract init logic to new package regolith-debootstick-setup

--- .github/scripts/gen-debootstick-rootfs.sh
+++ .github/scripts/gen-debootstick-rootfs.sh
@@ -22,24 +22,22 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --upgrade \
     wireless-tools \
     wpasupplicant
 
-# Enable first-boot system configuration
+# Locale generation - moving to regolith-debootstick-setup
 
-chmod +x /usr/bin/interactive-setup.sh
-systemctl enable interactive-setup.service
-
-# Locale generation
-
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+# echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+# sed -i 's/^# *\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+# locale-gen
 
 # Regolith Deb Repo 
 
 wget -qO - https://archive.regolith-desktop.com/regolith.key | gpg --dearmor | tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
-
+ 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://archive.regolith-desktop.com/debian/unstable trixie main" > /etc/apt/sources.list.d/regolith.list
-
+ 
 apt update
-
+ 
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    regolith-debootstick-setup \
     regolith-desktop \
     regolith-lightdm-config \
     regolith-look-lascaille \
